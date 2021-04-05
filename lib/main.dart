de4c40861a7cc64eb43e4f873bec033d237f9ca6
sofiabukovskaya <sofi454545@gmail.com>
2021-04-05 15:57:15 +0300

    Create class Person and constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  //task 1
 var name = myName();
 print('My name is $name');
+print('My name is ${name.length}');
+
+//task2
+var person = new Person('Sonya');
+person.printName();
 }
 
 String myName() {
   return 'Sonya';
 }
 
+class Person {
+    String firstName;
+
+    Person(this.firstName);
+
+    printName() {
+      print(firstName);
+    }
+}
